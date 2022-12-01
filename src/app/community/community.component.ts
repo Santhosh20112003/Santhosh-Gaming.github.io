f1c58d114781e33e082ae50c862c98785c625b8b
Santhosh <shanmugamsanthosh22@gmail.com>
2022-12-01 22:20:11 +0530

    cjnsj

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -9,8 +9,26 @@ import {} from "@google/model-viewer";
 
 export class CommunityComponent implements OnInit {
   public name="";
-  constructor() { }
+  isFavorite :boolean=false;
   
+  View="View";
+  More="More";
+  count= 1;
+  constructor() { }
+  display(){
+    if(this.count ==1){
+      this.isFavorite =true;
+      this.View='Show';
+      this.More='Less';
+      this.count=0;
+    }
+    else{
+      this.isFavorite =false;
+      this.View='View';
+      this.More='More';
+      this.count=1;
+    }
+  }
   ngOnInit(): void {
   }
 
