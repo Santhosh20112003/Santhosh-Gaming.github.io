8285462d62e65cd12e08680fbaf5ba047ffeb189
Santhosh <shanmugamsanthosh22@gmail.com>
2022-12-09 00:20:34 +0530

    jnedjne

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {} from "@google/model-viewer";
-import {faAngleUp ,faRobot,faHeart,faHouse,faPuzzlePiece,faUsers,faAlignRight} from '@fortawesome/free-solid-svg-icons'
-
+import {faAngleUp} from '@fortawesome/free-solid-svg-icons';
+import * as L from 'leaflet';
+import {Toast} from 'bootstrap';
 @Component({
   selector: 'app-community',
   templateUrl: './community.component.html',
@@ -30,7 +31,26 @@ export class CommunityComponent implements OnInit {
       this.count=1;
     }
   }
-  ngOnInit(): void {
-  }
+  private map:any;
 
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [51.505, -0.09],
+    zoom: 13
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+    tiles.addTo(this.map);
+  }
+  ngOnInit(): void {
+ 
+  }
+toast(value:any){
+  value.show();
 }
+}
+
